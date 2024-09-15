2d9d171d237934756c6a77bd998191d863409e80
HARSH PANDEY <102924941+harshpandey80@users.noreply.github.com>
2024-09-16 02:53:20 +0530

    Update dbconnect.js

--- Desktop/Fruiet/server/config/dbconnect.js
+++ Desktop/Fruiet/server/config/dbconnect.js
@@ -1,17 +1,19 @@
-const mongoose = require("mongoose");
-require("dotenv").config(); // Ensure environment variables are loaded
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const connectDB = async () => {
+const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    console.log("MongoDB connected successfully!");
+    console.log('Database connected successfully');
   } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
-    process.exit(1); // Exit process with failure if connection fails
+    console.log('Issue in DB Connection');
+    console.error(error.message);
+    // Exiting the process with a failure code
+    process.exit(1);
   }
 };
 
-module.exports = connectDB;
+module.exports = dbConnect;
