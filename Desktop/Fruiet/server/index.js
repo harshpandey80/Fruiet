30b2a32c88522f2b3f2656a9b2a77ccd9813218a
HARSH PANDEY <102924941+harshpandey80@users.noreply.github.com>
2024-09-16 04:14:42 +0530

    Update index.js

--- Desktop/Fruiet/server/index.js
+++ Desktop/Fruiet/server/index.js
@@ -38,5 +38,5 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
