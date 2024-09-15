9691030d27a8d3f54a65ffe4cd32d31703d016cb
HARSH PANDEY <102924941+harshpandey80@users.noreply.github.com>
2024-09-16 04:18:27 +0530

    Update dbconnect.js

--- Desktop/Fruiet/server/config/dbconnect.js
+++ Desktop/Fruiet/server/config/dbconnect.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('Database connected successfully');
   } catch (error) {
